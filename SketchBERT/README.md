7201e56c529dec6eee81bb48ea97486ae5fb565f
selfspin <1286011841@qq.com>
2022-06-15 15:23:55 +0800

    sf

---
+++ SketchBERT/README.md
@@ -0,0 +1,56 @@
+## Pre-train Model
+You can get the pretrained model on [Google Drive](https://drive.google.com/file/d/1y6-0RqzdqrExDkHC0BXOzIRUEl_Ei1da/view?usp=sharing).
+
+## Preparing the Dataset
+我们采用Oracle-source的无标注数据训练，在训练前，需要在`npz_sum.txt`文件中放入训练数据地址，例如
+
+```
+../Oracle/data/oracle_source/oracle_source_seq/oracle_source_seq.npz
+```
+
+然后运行`generate_dataset.py`文件，获得生成的数据集
+
+## Training
+
+训练模型
+
+```shell
+python main.py train models/SketchTransformer/config/sketch_transformer.yml
+```
+
+训练之前您需要在 `models/SketchTransformer/config/sketch_transformer.yml` 中设置好生成的数据集的路径，例如
+
+```
+sum_path: 'Oracle/memmap_sum.txt'
+offset_path: 'Oracle/offsets.npz'
+```
+
+以及预训练模型的路径
+
+```
+restore_checkpoint_path: 'checkpoint/pre-train.pth.tar'
+```
+
+最终模型在`model_logs`文件夹中
+
+我们训练好的模型在 `checkpoint/best_ckpt.pth.tar` 中
+
+## Generate
+
+先运行甲骨文识别网络文件夹`../Oracle`中的`data/getclass.py`, 其会将甲骨文和数字一一对应，并获得两个`.npy`文件在`data/oracle_fs/img/`文件夹下
+
+运行`generate_oraclefs.py `获得生成的数据集
+
+生成Oracle-FS的数据增强结果
+
+```
+python augmentor test models/SketchTransformer/config/generate_oracle_fs.yml
+```
+
+运行之前您需要在 `models/SketchTransformer/config/generate_oracle_fs.yml` 中设置好训练完的最终模型地址，例如
+
+```
+restore_checkpoint_path: 'checkpoint/best_ckpt.pth.tar'
+```
+
+生成结果在 `Generate_oracle_fs/` 文件夹下，请把这整个文件夹放到甲骨文识别网络文件夹中的`data/` 下
---
+++ SketchBERT/models/SketchTransformer/models/generate_dataset.py
@@ -0,0 +1,64 @@
+import torch
+import numpy as np
+import os
+import pickle as pkl
+import torch
+from PIL import Image
+
+sum_path = 'npz_sum.txt'
+# a file with all npz names in quickdraw, a line with a path.
+modes = ['train', 'valid', 'test']
+offsets = {'train': {}, 'valid': {}, 'test': {}}
+save_dir = 'QuickDraw/memmap'  # simple_memmap
+load_file = {'full': open('QuickDraw/memmap_sum.txt', 'w')}
+max_len = 0
+
+
+def get_class(path):
+    return path[path.rfind('/') + 1:-4]
+
+
+with open(sum_path, 'r') as rf:
+    for line in rf:
+        data_path = line.strip().split('\t')[0]
+        # data_path = data_path.replace('/npz/','/npz/') #simple_npz
+        if not os.path.exists(data_path):
+            continue
+        data = np.load(data_path, encoding='latin1', allow_pickle=True)
+        tmp_data = []
+        tmp_num = 0
+        for mode in modes:
+            for sketch in data[mode]:
+                tmp_data.append(sketch)
+            cls_name = get_class(data_path)
+            save_path = os.path.join(save_dir, '{}_{}.dat'.format(cls_name, mode))
+            offsets[mode][cls_name] = []
+            start = 0
+            max_len = 0
+            len_record = []
+            for sketch in tmp_data:
+                if len(sketch.shape) != 2 or sketch.shape[1] != 3:
+                    print(sketch)
+                    continue
+                end = start + sketch.shape[0]
+                len_record.append(sketch.shape[0])
+                # print(sketch.shape)
+                max_len = max(max_len, sketch.shape[0])
+                offsets[mode][cls_name].append((start, end))
+                start = end
+            len_record = np.array(len_record)
+            tmp_num += len(tmp_data)
+            print(mode, 'mean:{}, std:{}, max:{}, min:{}'.format(len_record.mean(), len_record.std(), len_record.max(),
+                                                                 len_record.min()))
+            max_len = 250
+            print('Num Count: < {} :{}, total:{}'.format(max_len, (len_record < max_len).sum(), len(len_record)))
+            # print(save_path)
+
+            stack_data = np.concatenate(tmp_data, axis=0)
+            print(stack_data.dtype, stack_data)
+            tmp_memmap = np.memmap(save_path, dtype=np.int16, mode='write', shape=stack_data.shape)
+            tmp_memmap[:] = stack_data[:]
+            tmp_memmap.flush()
+        load_file['full'].write('{}\t{}\n'.format(os.path.join(save_dir, '{}.dat'.format(cls_name)), tmp_num))
+#
+pkl.dump(offsets, open('QuickDraw/offsets.npz', 'wb'))
